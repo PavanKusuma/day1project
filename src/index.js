1b6fe59c007e0de126623656c50680d17eb35b91
Pavan Kusuma <newtonpavan33@gmail.com>
2024-07-26 14:40:02 +0530

    Deploy commit2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 import ReactDOM  from "react-dom/client";
 
-import Seven from "./Seven";
-import CardUI from "./Card";
-import CardListUI from "./CardList";
-import Eight from "./Eight";
-import Nine from "./Nine";
-import Ten from "./Ten";
-import Eleven from "./Eleven";
-import Login from "./Login";
-import Onboard from "./Onboard";
-import Register from "./Register";
-import DataFetching from "./DataFetching";
-import DataFetching2 from "./DataFetching2";
-import DataFetching3 from "./DataFetching3";
 import App from "./App";
 
 const rootElement = document.getElementById('root')
